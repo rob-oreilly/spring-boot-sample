68e4de3370eed88d56b7d31fa1733d085390ff9f
Robert O'Reilly <roboreilly2017@gmail.com>
2017-11-02 18:57:26 +0000

    Spring boot sample

---
+++ src/main/java/com/codeforge/springboot/SampleController.java
@@ -0,0 +1,14 @@
+package com.codeforge.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "hello world!";
+    }
+}
